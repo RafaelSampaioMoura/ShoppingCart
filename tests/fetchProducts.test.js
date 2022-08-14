9386c427453a1eec15aa624ced3f62d4c66140d1
HailMuffins <rafaelsm123@outlook.com>
2022-08-13 21:14:24 -0300

    Requisito 1 Iniciado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,29 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+//gogogo
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('fetchProducts é uma função', () => {
+    expect(fetchProducts()).toBeInstanceOf(Function);
+  });
+
+  test('Verifica se fetch foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+
+  test('Verifica de fetch foi chamado com o parâmetro correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  test('Verifica se a função retorna o resultado esperado', async () => {
+    await expect(fetchProducts('computador')).resolve.toEqual(computadorSearch);
+  });
+
+  test('Verifica se o erro está correto', async () => {
+    await expect(fetchProducts()).reject.toMatch("Shit went tits up, my man");
+  })
 });
