e3b5b288af603fcc63f6d291ecfb5edcefec0c41
HailMuffins <rafaelsm123@outlook.com>
2022-08-15 13:02:29 -0300

    Requisito 8

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,7 @@
 const saveCartItems = () => {
   // seu código aqui
+  const cartToSave = document.querySelector('ol.cart__items').innerHTML;
+  localStorage.setItem('cartItems', cartToSave);
 };
 
 if (typeof module !== 'undefined') {
