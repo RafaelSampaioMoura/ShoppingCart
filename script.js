940a49c19cc1ffaed4a1ca60af51b8a240570592
HailMuffins <rafaelsm123@outlook.com>
2022-08-15 19:56:23 -0300

    Projeto Completado, Lint Apaziguado

--- script.js
+++ script.js
@@ -2,22 +2,39 @@ const computerList = document.querySelector('section .items');
 const clearBtn = document.querySelector('button.empty-cart');
 const shoppingList = document.querySelector('ol.cart__items');
 
+// const carregado = () => {
+//   const body = document.querySelector('body');
+//   body.remove();
+// }
+const totalPrice = () => {
+  const showPrice = document.querySelector('span.total-price');
+  const cartItems = document.querySelectorAll('li.cart__item');
+  let sum = 0;
+  cartItems.forEach((item) => {
+    const thePrice = item.textContent.split('$');
+    sum += Number(thePrice[1]);
+  });
+  const sumText = sum.toLocaleString(2).split('.').join('').replace(/,/g, '.');
+  showPrice.innerText = `${sumText}`;
+};
 const clearCart = () => {
-  const theCart = document.querySelector('ol.cart__items');
+  const theCart = shoppingList;
   theCart.innerHTML = '';
-  saveCartItems(document.querySelector('ol.cart__items').innerHTML);
+  saveCartItems(shoppingList.innerHTML);
+  totalPrice();
 };
 
 clearBtn.addEventListener('click', clearCart);
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
   event.target.remove();
+  saveCartItems(shoppingList.innerHTML);
   totalPrice();
 };
 
 const restablishCart = () => {
   const theCart = getSavedCartItems();
-  document.querySelector('ol.cart__items').innerHTML = theCart;
+  shoppingList.innerHTML = theCart;
   const cartItems = document.querySelectorAll('li.cart__item');
   cartItems.forEach((item) => {
     item.addEventListener('click', cartItemClickListener);
@@ -29,6 +46,19 @@ const createProductImageElement = (imageSource) => {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
+};
+
+const carregando = () => {
+  const items = document.querySelector('section.items');
+  const loading = document.createElement('p');
+  loading.className = 'loading';
+  loading.innerText = 'carregando...';
+  items.appendChild(loading);
+};
+
+const carregado = () => {
+  const remove = document.querySelector('p.loading');
+  remove.remove();
 };
 
 const createCustomElement = (element, className, innerText) => {
@@ -50,55 +80,40 @@ const createCartItemElement = async (e) => {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   theCartList.appendChild(li);
-  saveCartItems(document.querySelector('ol.cart__items').innerHTML);
+  saveCartItems(shoppingList.innerHTML);
   totalPrice();
 };
 
-const createProductItemElement = async (sku, name, image) => {
-  const response = await fetchProducts('computador');
-  const computers = response.results;
-
-  computers.forEach((computer) => {
+const getItemsFromApi = (items) => {
+  items.forEach((computer) => {
     const section = document.createElement('section');
     section.className = 'item';
-    sku = computer.id;
-    name = computer.title;
-    image = computer.thumbnail;
-    section.appendChild(createCustomElement('span', 'item__sku', sku));
-    section.appendChild(createCustomElement('span', 'item__title', name));
-    section.appendChild(createProductImageElement(image));
+    section.appendChild(createCustomElement('span', 'item__sku', computer.id));
     section.appendChild(
-      createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
+      createCustomElement('span', 'item__title', computer.title),
+    );
+    section.appendChild(createProductImageElement(computer.thumbnail));
+    section.appendChild(
+      createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
     );
     const cartBtns = document.querySelectorAll('.item__add');
     cartBtns.forEach((btn) =>
-      btn.addEventListener('click', createCartItemElement)
-    );
-
+      btn.addEventListener('click', createCartItemElement));
     computerList.appendChild(section);
     restablishCart();
   });
 };
 
-// const getSkuFromProductItem = (item) =>
-//   item.querySelector('span.item__sku').innerText;
-
-const totalPrice = () => {
-  const theCart = document.querySelector('ol.cart__items');
-  const showPrice = document.querySelector('span.total-price');
-  const cartItems = document.querySelectorAll('li.cart__item');
-  let sum = 0;
-  cartItems.forEach((item) => {
-    const thePrice = item.textContent.split('$');
-    sum += Number(thePrice[1]);
-    showPrice.textContent = sum;
-  });
-  showPrice.innerText = `Preço Total: ${sum.toLocaleString(2)}`;
-  theCart.appendChild(showPrice);
+const createItem = async () => {
+  carregando();
+  const response = await fetchProducts('computador');
+  carregado();
+  const computers = response.results;
+  getItemsFromApi(computers);
 };
 
 window.onload = () => {
-  createProductItemElement();
+  // carregando();
+  // carregado();
+  createItem();
 };
-// window.onunload = () =>
-//   ;
